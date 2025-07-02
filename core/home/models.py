c66db51b8459825b1b6490bfdc556648d83ae042
Manikanta Gandepalli <manikantagandepalli456@gmail.com>
2025-07-02 18:18:23 +0530

    "Added the foreign key data serialization changes"

--- core/home/models.py
+++ core/home/models.py
@@ -3,7 +3,18 @@ from django.db import models
 
 
 # Create your models here.
+class Color(models.Model):
+    color_name = models.CharField(max_length=100)
+
+
+    def __str__(self):
+        return self.color_name
 
 class Person(models.Model):
     name = models.CharField(max_length= 100)
     age = models.IntegerField()
+    color = models.ForeignKey(Color, on_delete=models.CASCADE, null=True, blank=True,related_name="color" )
+
+
+
+
--- core/home/serializers.py
+++ core/home/serializers.py
@@ -1,12 +1,20 @@
 from rest_framework import serializers
 
-from .models import Person
+from .models import Person, Color
+
+
+class ColorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Color
+        fields = ["color_name"]
 
 
 class PersonSerializer(serializers.ModelSerializer):
+    color = ColorSerializer()
 
     class Meta:
         model = Person
         fields = '__all__'
+        #depth = 1
 
 
